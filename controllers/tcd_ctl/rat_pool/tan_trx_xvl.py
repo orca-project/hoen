ca417c9901bf9da1c3b7728af82c572064acb3d9
Joao F. Santos <facocalj@tcd.ie>
2019-03-11 17:17:00 +0000

    Graciously exit and send free message

--- controllers/tcd_ctl/rat_pool/tan_trx_xvl.py
+++ controllers/tcd_ctl/rat_pool/tan_trx_xvl.py
@@ -17,86 +17,105 @@ import argparse
 import hydra
 import threading
 
-
 import signal
 
-def tan_trx_zmq(**kwargs):
-    # Top block instance
-    tb = gr.top_block()
-    ##################################################
-    # Variables
-    ##################################################
-    xvl_host = kwargs.get('xvl_host', '127.0.0.1')
-    xvl_port = kwargs.get('xvl_port', 5000)
-    tx_offset = kwargs.get('tx_offset', -1e6)
-    rx_offset = kwargs.get('rx_offset', +1e6)
-    samp_rate = kwargs.get('samp_rate', 1e6)
-    centre_frequency = kwargs.get('centre_freq', 3.75e9)
-    rat_id = kwargs.get('rat_id', 1)
-    payload_size = kwargs.get('payload_size', 1000)
 
-    ##################################################
-    # Blocks
-    ##################################################
-    hydra_gr_sink_0 = hydra.hydra_gr_client_sink(rat_id, xvl_host, xvl_port)
+class tan_trx_zmq(object):
+    def __init__(self, **kwargs):
+        # Top block instance
+        self.tb = gr.top_block()
+        ##################################################
+        # Variables
+        ##################################################
+        self.xvl_host = kwargs.get('xvl_host', '127.0.0.1')
+        self.xvl_port = kwargs.get('xvl_port', 5000)
+        self.tx_offset = kwargs.get('tx_offset', -1e6)
+        self.rx_offset = kwargs.get('rx_offset', +1e6)
+        self.samp_rate = kwargs.get('samp_rate', 1e6)
+        self.centre_frequency = kwargs.get('centre_freq', 3.75e9)
+        self.rat_id = kwargs.get('rat_id', 1)
+        self.payload_size = kwargs.get('payload_size', 1000)
 
-    hydra_gr_sink_0.start_client(centre_frequency + tx_offset, samp_rate,
-                                 payload_size)
-    print('tx', centre_frequency + tx_offset)
-    hydra_gr__source_0_0 = hydra.hydra_gr_client_source(
-        rat_id, xvl_host, xvl_host, xvl_port)
+    def run(self):
+        ##################################################
+        # Blocks
+        ##################################################
+        self.hydra_gr_sink_0 = hydra.hydra_gr_client_sink(
+            self.rat_id, self.xvl_host, self.xvl_port)
 
-    print('rx', centre_frequency + rx_offset)
-    hydra_gr__source_0_0.start_client(centre_frequency + rx_offset, samp_rate,
-                                      payload_size)
+        self.hydra_gr_sink_0.start_client(
+            self.centre_frequency + self.tx_offset, self.samp_rate,
+            self.payload_size)
 
-    digital_ofdm_tx_0 = digital.ofdm_tx(
-        fft_len=64,
-        cp_len=16,
-        packet_length_tag_key='packet_len',
-        bps_header=1,
-        bps_payload=1,
-        rolloff=0,
-        debug_log=False,
-        scramble_bits=False)
+        self.hydra_gr_source_0_0 = hydra.hydra_gr_client_source(
+            self.rat_id, self.xvl_host, self.xvl_host, self.xvl_port)
 
-    digital_ofdm_rx_0 = digital.ofdm_rx(
-        fft_len=64,
-        cp_len=16,
-        frame_length_tag_key='frame_' + 'packet_len',
-        packet_length_tag_key='packet_len',
-        bps_header=1,
-        bps_payload=1,
-        debug_log=False,
-        scramble_bits=False)
+        self.hydra_gr_source_0_0.start_client(
+            self.centre_frequency + self.rx_offset, self.samp_rate,
+            self.payload_size)
 
-    blocks_tuntap_pdu_0 = blocks.tuntap_pdu('tap' + str(rat_id), 1000, True)
+        print('tx', self.centre_frequency + self.tx_offset)
+        print('rx', self.centre_frequency + self.rx_offset)
 
-    blocks_tagged_stream_to_pdu_0 = blocks.tagged_stream_to_pdu(
-        blocks.byte_t, 'packet_len')
+        self.digital_ofdm_tx_0 = digital.ofdm_tx(
+            fft_len=64,
+            cp_len=16,
+            packet_length_tag_key='packet_len',
+            bps_header=1,
+            bps_payload=1,
+            rolloff=0,
+            debug_log=False,
+            scramble_bits=False)
 
-    blocks_pdu_to_tagged_stream_0 = blocks.pdu_to_tagged_stream(
-        blocks.byte_t, 'packet_len')
+        self.digital_ofdm_rx_0 = digital.ofdm_rx(
+            fft_len=64,
+            cp_len=16,
+            frame_length_tag_key='frame_' + 'packet_len',
+            packet_length_tag_key='packet_len',
+            bps_header=1,
+            bps_payload=1,
+            debug_log=False,
+            scramble_bits=False)
 
-    ##################################################
-    # Connections
-    ##################################################
-    tb.msg_connect((blocks_tagged_stream_to_pdu_0, 'pdus'),
-                   (blocks_tuntap_pdu_0, 'pdus'))
+        self.blocks_tuntap_pdu_0 = blocks.tuntap_pdu('tap' + str(self.rat_id),
+                                                     1000, True)
 
-    tb.msg_connect((blocks_tuntap_pdu_0, 'pdus'),
-                   (blocks_pdu_to_tagged_stream_0, 'pdus'))
+        self.blocks_tagged_stream_to_pdu_0 = blocks.tagged_stream_to_pdu(
+            blocks.byte_t, 'packet_len')
 
-    tb.connect((blocks_pdu_to_tagged_stream_0, 0), (digital_ofdm_tx_0, 0))
+        self.blocks_pdu_to_tagged_stream_0 = blocks.pdu_to_tagged_stream(
+            blocks.byte_t, 'packet_len')
 
-    tb.connect((digital_ofdm_rx_0, 0), (blocks_tagged_stream_to_pdu_0, 0))
+        ##################################################
+        # Connections
+        ##################################################
+        self.tb.msg_connect((self.blocks_tagged_stream_to_pdu_0, 'pdus'),
+                            (self.blocks_tuntap_pdu_0, 'pdus'))
 
-    tb.connect((digital_ofdm_tx_0, 0), (hydra_gr_sink_0, 0))
+        self.tb.msg_connect((self.blocks_tuntap_pdu_0, 'pdus'),
+                            (self.blocks_pdu_to_tagged_stream_0, 'pdus'))
 
-    tb.connect((hydra_gr__source_0_0, 0), (digital_ofdm_rx_0, 0))
+        self.tb.connect((self.blocks_pdu_to_tagged_stream_0, 0),
+                        (self.digital_ofdm_tx_0, 0))
 
-    # Start the boombox
-    tb.run()
+        self.tb.connect((self.digital_ofdm_rx_0, 0),
+                        (self.blocks_tagged_stream_to_pdu_0, 0))
+
+        self.tb.connect((self.digital_ofdm_tx_0, 0), (self.hydra_gr_sink_0, 0))
+
+        self.tb.connect((self.hydra_gr_source_0_0, 0),
+                        (self.digital_ofdm_rx_0, 0))
+
+        # Start the boombox
+        self.tb.start()
+
+    def exit(self, *args):
+        self.tb.stop()
+        self.tb.wait()
+
+        del self.hydra_gr_sink_0
+        del self.hydra_gr_source_0_0
+
 
 
 def get_args():
@@ -130,4 +149,11 @@ if __name__ == '__main__':
     # Get CLI arguments as a dictionary
     kwargs = get_args()
     # Call the main function
-    tan_trx_zmq(**kwargs)
+    k = tan_trx_zmq(**kwargs)
+
+    signal.signal(signal.SIGINT, k.exit)
+    signal.signal(signal.SIGTERM, k.exit)
+
+    k.run()
+
+    signal.pause()
